24e32f93ec30ff67dd771bf377d3704fd7f14e93
AdItYa302007 <66154816+AdItYa302007@users.noreply.github.com>
2021-02-22 20:00:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDwZkTMSatvYNINMC0AgX0NRwylqYiyNjc",
+    authDomain: "barter-app-31ac5.firebaseapp.com",
+    projectId: "barter-app-31ac5",
+    storageBucket: "barter-app-31ac5.appspot.com",
+    messagingSenderId: "10691061643",
+    appId: "1:10691061643:web:1c38bcd94332024848b18e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
